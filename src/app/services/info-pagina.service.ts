e6b2589afc49693bb26701ba998fbd1b1f95c283
Leandro Fuschetto <leandro.fuschetto@gmail.com>
2022-05-13 19:07:07 -0300

    fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,19 +9,33 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = []; 
 
   constructor( private http: HttpClient) {
       console.log('pruebita log')
+      this.CargaInfo();
+      this.CargarEquipo();
+   }
+
+   private CargaInfo() {
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
+
+      this.cargada = true;
+      this.info = resp;
+
+      console.log( resp.twitter );
+
+    });
+   }
+
+   private CargarEquipo() {
+    this.http.get('https://angular-html-9cfec-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => {
+
+      this.equipo = resp;
       
-      //leer el archivo json
-      this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
-
-        this.cargada = true;
-        this.info = resp;
-        
-        console.log( resp.twitter );
-
-      });
+    });
    }
 }
+
